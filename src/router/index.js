e9eaf9b992b54598b1d3ba5a451a59dca71fa363
Marvin Janitra Akmal <86354527+marvinjanitraa@users.noreply.github.com>
2021-07-05 12:57:53 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Courses from '../views/Courses.vue'
 import Mahasiswa from '../views/Mahasiswa.vue'
+import regisD from '../views/regisD.vue'
+import regisM from '../views/regisM.vue'
+import regisP from '../views/regisP.vue'
+import loginP from '../views/loginP.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +18,22 @@ const routes = [
     path: '/mahasiswa',
     name: 'Mahasiswa',
     component: Mahasiswa
+  },
+  {
+    path: '/',
+    component: regisP
+  },
+  {
+    path: '/login',
+    component: loginP
+  },
+  {
+    path: '/regisD',
+    component: regisD
+  },
+  {
+    path: '/regisM',
+    component: regisM
   }
 ]
 
